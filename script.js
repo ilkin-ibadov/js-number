dffc546dc4a607817f8b257581761331aa00e8df
Ilkin Ibadov <ilkin.ibadov21@gmail.com>
2025-10-21 16:22:14 +0400

    update

--- script.js
+++ script.js
@@ -1,59 +1,59 @@
-let a = "   Hello World.  "
-let txtWithoutPadding = "javascript"
+let a = "   Hello World!   "
+console.log(a.length) // get string's character count
 
-console.log(a.length) // returns number of characters in a string
+// get character at given index
+console.log(a.at(0)) 
+console.log(a[0])
 
-console.log(a.trim()) // removes spaces from beginning and end of string
-
-const paddedTextStart = txtWithoutPadding.padStart(txtWithoutPadding.length + 3, " ") // adds given string to the beginning given times
-const paddedTextEnd = txtWithoutPadding.padEnd(txtWithoutPadding.length + 3, " ") // adds given string to the end given times
-
-// for merging strings together
+// merge strings together
 let text1 = "Hello";
 let text2 = "World";
-let mergedTextUsingMethod = text1.concat(" ", text2);
-let mergedText = text1 + " " + text2
+let text3 = text1.concat(" ", text2);
+// console.log(text1 + " " + text2)
 
-// for getting character at given index 
-let selectedIndexUsingMethod = text1.at(2)
-let selectedIndex = text1[2]
+console.log(a.trim()) // trim() removes whitespace from both ends of string
+// trimStart() removes whitespace from beginning of string
+// trimEnd() removes whitespace from end of string
 
-let partOfString = text1.slice(0, 2) // returns part of string slice(startIndex, endIndex) endIndex is not included
-// if endIndex is not given, starting from startIndex, returns rest of the string to the end
-// if startIndex and endIndex is negative, process works from opposite side
-// substring() method is similar to slice() difference is startIndex and endIndex cannot be negative
+let paddedStringStart = text1.padStart(text1.length + 3, " ") // adds given character to the beginning of string given times
+let paddedStringEnd = text1.padEnd(text1.length + 3, " ") // adds given character to the end of string given times
 
-// to transform text to either uppercase or lowercase
-let lowercaseTxt = "hello"
+console.log(text2.repeat(3)) // repeats string given times
 
-console.log(lowercaseTxt)
+let text = "Apple, Banana, Kiwi";
+let part = text.slice(-12, -6);
+// slice(startIndex, endIndex)
+// endIndex is optional, if not present, slice() returns string value after given index
+// if indexes are negative, we count indexes from right
 
-let uppercaseTxt = lowercaseTxt.toUpperCase()
+// substring() exactly same as slice, only difference is it does not accept negative indexes
 
-console.log(uppercaseTxt)
+let txtToUppercase = text1.toUpperCase() // string to uppercase
 
-let lowercaseAgain = uppercaseTxt.toLocaleLowerCase()
+let txtToLowercase = txtToUppercase.toLowerCase() // string to lowercase
 
-console.log(lowercaseAgain)
+let text5 = "Please visit Microsoft!";
+let newText = text5.replace("Microsoft", "W3Schools"); // replaces given text with second parameter
+// we can use "" as second parameter to remove a word
+// replaceAll() replaces all matching words with given word
 
-// to repeat text given times
-let text3 = "Hello world!";
-let result = text3.repeat(4);
+let sentenceToWords = text5.split(" ") // splits sentence to words, returns array
+let wordToCharacters = text1.split("")
 
-// to replace part of text with another text
-let text4 = "Please visit Microsoft and Microsoft!";
-let replacedText = text4.replace("Microsoft", "StepIT");
-let replacedAllText = text4.replaceAll("Microsoft", "StepIT");
+console.log(wordToCharacters)
 
+console.log(text5.includes("visit")) // returns true if string contains given word, else returns false
 
-let txtToWords = text4.split(" ") // Splits string to parts by given character, outputs an array
-console.log(txtToWords)
+console.log(text5.startsWith("Please")) // returns true if string starts with given word, else returns false
+// endsWith() method returns true if string ends with given word, else returns false
 
-let txtIncludesWord = text4.includes("Microsoft") // checks if string contains given word
-let startSearchingFromIndex = text4.includes("Please", 6) // starts checking from given index
+// indexOf() returns starting index of first matching word, lastIndexOf() returns starting index of last matching word
 
-let text = "Please locate where 'locate' occurs!"; // searches text, returns starting index of given word
-text.search("locate");
+// Both indexOf() and lastIndexOf() accepts optional second parameter, if given starts searching from that index
 
-let text5 = "The rain in SPAIN stays mainly in the plain"; // searches for any match, multiple locations, case-insensitive
-console.log(text5.match(/ain/gi))
+// Both indexOf() and lastIndexOf() returns -1 if no match is found
+
+console.log("indexOf: ", text5.indexOf('microsoft')) // returns false, does not accept regular expression
+
+console.log("search: ", text5.search(/microsoft/i)) // returns true, can work with both lowercase and uppercase
+
